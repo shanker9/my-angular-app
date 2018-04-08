1eaf41afdee92302e9e20bb98e0e5ad8d0a349eb
shanker9 <ssnani9@gmail.com>
2018-04-09 00:40:59 +0530

    initial commit

---
+++ src/app/ImageViewer/imageViewer.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector : 'image-viewer',
+    templateUrl : './imageViewer.component.html'
+})
+export default class ImageViewer {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'My Angular App';
+  name = 'sunset';
+  randomnames = '';
+  ngOnInit() {
+  }
+
+  setNames() {
+    var nameList = ['Jamal Boss', 'Margery Arend', 'Jamee Rosamond', 'Mellisa Boots', 'Don Wegner', 'Michal Mellish', 'Joi Kohn', 'Ron Tappan', 'Kory Thaler', 'Myron Theriault', 'Landon Hulet', 'Marylee Tuller', 'Maricela Swaby', 'Kathrin Lefebure', 'Tosha Dauphin', 'Quincy Nobile', 'Buena Chavous', 'Jadwiga Tomblin', 'Artie Ory', 'Maisha Naquin', 'Terrell Loughlin', 'Wendy Strachan', 'Dina Staley', 'Lizzie Trojan', 'Adalberto Tabler', 'Matilda Hudock', 'Veronica Fillion', 'Anton Luff', 'Ericka Cochran', 'Amparo Boster', 'Freida Connally', 'Mae Binney', 'Wanita Sutera', 'Rolland Bartel', 'Miki Tanouye', 'Sueann Revelle', 'Dannielle Zacharias', 'Sixta Crosland', 'Darnell Treece', 'Hyo Woodman', 'Alexis Savarese', 'Charita Machnik', 'Trena Whitwell', 'Emerald Stutts', 'Mckinley Grillo', 'Allen Bradfield', 'Chun Kwan', 'Ying Leiser', 'Jamika Bucy', 'Tena Schisler'];
+
+    // setInterval(() => {
+    //   this.randomnames = `${nameList[Math.floor(Math.random() * 50)]}`;
+    // }, 1000)
+  }
 }
